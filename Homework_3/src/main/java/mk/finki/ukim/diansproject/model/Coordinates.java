6e8f61a6f39276a88ad5a8564d19ce6608b44106
bilef23 <92641392+bilef23@users.noreply.github.com>
2023-12-21 20:50:27 +0100

    Making coordinates class entity and adding the place

--- Homework_3/src/main/java/mk/finki/ukim/diansproject/model/Coordinates.java
+++ Homework_3/src/main/java/mk/finki/ukim/diansproject/model/Coordinates.java
@@ -1,17 +1,28 @@
 package mk.finki.ukim.diansproject.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Generated;
 
 @Data
+@Entity
 public class Coordinates {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     Double latitude;
     Double longitude;
+    @OneToOne
+    CulturalPlace place;
 
-    public Coordinates(Double latitude, Double longitude) {
+
+    public Coordinates(Double latitude, Double longitude,CulturalPlace place) {
         this.latitude = latitude;
         this.longitude = longitude;
+        this.place=place;
     }
 
     public Coordinates() {
     }
+
 }
