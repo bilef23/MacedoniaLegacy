af02248ae5c5dc9d745bc15b256f35bcc8d9f935
bilef23 <92641392+bilef23@users.noreply.github.com>
2023-12-22 00:29:57 +0100

    removing geocoding service import

--- Homework_3/src/main/java/mk/finki/ukim/diansproject/web/LocationController.java
+++ Homework_3/src/main/java/mk/finki/ukim/diansproject/web/LocationController.java
@@ -4,7 +4,6 @@ import mk.finki.ukim.diansproject.model.Coordinates;
 import mk.finki.ukim.diansproject.model.CulturalPlace;
 import mk.finki.ukim.diansproject.service.CoordinatesService;
 import mk.finki.ukim.diansproject.service.CulturalPlaceService;
-import mk.finki.ukim.diansproject.service.GeocodingService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,24 +14,21 @@ import java.io.IOException;
 @CrossOrigin(origins = "http://localhost:9090")
 public class LocationController {
     private final CulturalPlaceService culturalPlaceService;
-    private final GeocodingService geocodingService;
     private final CoordinatesService coordinatesService;
 
-    public LocationController(CulturalPlaceService culturalPlaceService, GeocodingService geocodingService, CoordinatesService coordinatesService) {
+    public LocationController(CulturalPlaceService culturalPlaceService, CoordinatesService coordinatesService) {
         this.culturalPlaceService = culturalPlaceService;
-        this.geocodingService = geocodingService;
         this.coordinatesService = coordinatesService;
     }
     @PostMapping("/receiveLoc")
     @ResponseBody
-    public String receiveLocation(@RequestParam String latitude,
-                                  @RequestParam String longitude,
-                                  @RequestParam Long placeId) throws IOException {
+    public String receiveLocation(@RequestParam Long placeId) throws IOException {
         CulturalPlace place=culturalPlaceService.findById(placeId);
         Coordinates c=coordinatesService.findByPlace(place);
         double lat=c.getLatitude();
         double lon=c.getLongitude();
-
+        System.out.println(lat);
+        System.out.println(lon);
         return "{\"latitude\": "+lat+", \"longitude\": "+lon+" }";
     }
 }
