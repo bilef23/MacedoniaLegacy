3ab3b88ca99a317cc2e6b2acc0a09d07e040afd0
bilef23 <92641392+bilef23@users.noreply.github.com>
2023-12-24 20:55:25 +0100

    Initializing new RegisterController for handling registration requests

---
+++ Homework_3/src/main/java/mk/finki/ukim/diansproject/web/RegisterController.java
@@ -0,0 +1,53 @@
+package mk.finki.ukim.diansproject.web;
+
+import mk.finki.ukim.diansproject.model.Exceptions.InvalidArgumentsException;
+import mk.finki.ukim.diansproject.model.Exceptions.PasswordsDoNotMatchException;
+import mk.finki.ukim.diansproject.model.Exceptions.UsernameAlreadyExistsException;
+import mk.finki.ukim.diansproject.model.Role;
+import mk.finki.ukim.diansproject.service.AuthService;
+import mk.finki.ukim.diansproject.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping()
+public class RegisterController {
+    private final AuthService authService;
+    private final UserService userService;
+
+    public RegisterController(AuthService authService, UserService userService) {
+        this.authService = authService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage(@RequestParam(required = false) String error, Model model) {
+        if(error != null && !error.isEmpty()) {
+            model.addAttribute("hasError", true);
+            model.addAttribute("error", error);
+        }
+
+        return "signin.html";
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestParam String username,
+                           @RequestParam String password,
+                           @RequestParam String repeatedPassword,
+                           @RequestParam String name,
+                           @RequestParam String email
+    ) {
+        try{
+            System.out.println("OKS");
+            this.userService.register(username, password, repeatedPassword, name, email, Role.USER);
+            return "redirect:/login";
+        } catch (InvalidArgumentsException | PasswordsDoNotMatchException | UsernameAlreadyExistsException exception) {
+            return "redirect:/register?error=" + exception.getMessage();
+        }
+    }
+
+}
