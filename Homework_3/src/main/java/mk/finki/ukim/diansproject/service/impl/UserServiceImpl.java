a989ed2f04219f1a13b72343bb7e84e11c02ae73
bilef23 <92641392+bilef23@users.noreply.github.com>
2023-12-24 20:49:13 +0100

    Implementing userService methods

--- Homework_3/src/main/java/mk/finki/ukim/diansproject/service/impl/UserServiceImpl.java
+++ Homework_3/src/main/java/mk/finki/ukim/diansproject/service/impl/UserServiceImpl.java
@@ -1,14 +1,20 @@
 package mk.finki.ukim.diansproject.service.impl;
 
+import mk.finki.ukim.diansproject.model.Exceptions.InvalidArgumentsException;
+import mk.finki.ukim.diansproject.model.Exceptions.PasswordsDoNotMatchException;
+import mk.finki.ukim.diansproject.model.Exceptions.UsernameAlreadyExistsException;
 import mk.finki.ukim.diansproject.model.Role;
 import mk.finki.ukim.diansproject.model.User;
 import mk.finki.ukim.diansproject.repository.UserRepository;
 import mk.finki.ukim.diansproject.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -17,26 +23,34 @@ public class UserServiceImpl implements UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-
-    private boolean searchUserByUsernameAndPassword(String username, String password) {
-
-        return userRepository.searchUserByUsernameAndPassword(username,password)!=null;
-    }
-
-
-    private boolean searchUserByEmailAndPassword(String email, String password) {
-        return userRepository.searchUserByEmailAndPassword(email,password)!=null;
-    }
-
     @Override
-    public boolean isAuthenticated(String user, String password) {
-        return searchUserByEmailAndPassword(user,password)||searchUserByUsernameAndPassword(user,password);
-    }
-
-    @Override
-    public void addUser(String name, String username, String email, String password) {
-        if(!name.isEmpty() && !username.isEmpty() && !email.isEmpty() && !password.isEmpty()){
-            userRepository.save(new User(name,username,passwordEncoder.encode(password),email, Role.USER));
+    public User register(String username, String password, String repeatPassword, String name, String email, Role role) {
+        if (username == null || password == null || username.isEmpty() || password.isEmpty()) {
+            throw new InvalidArgumentsException();
         }
+
+        if (!password.equals(repeatPassword)) {
+            throw new PasswordsDoNotMatchException();
+        }
+
+        if(this.userRepository.findByUsername(username).isPresent()) {
+            throw new UsernameAlreadyExistsException(username);
+        }
+
+        User user = new User(
+                name,
+                username,
+                passwordEncoder.encode(password),
+                email,
+                role
+        );
+
+        return userRepository.save(user);
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(()->new UsernameNotFoundException(username));
+    }
+
 }
