bdb7af81ae81cde011648b45cef0efa4b82e9178
bilef23 <92641392+bilef23@users.noreply.github.com>
2023-12-24 20:52:42 +0100

    Changing the userService with authService and modifying the methods

--- Homework_3/src/main/java/mk/finki/ukim/diansproject/web/LoginController.java
+++ Homework_3/src/main/java/mk/finki/ukim/diansproject/web/LoginController.java
@@ -1,45 +1,46 @@
 package mk.finki.ukim.diansproject.web;
 
+import jakarta.servlet.http.HttpServletRequest;
+import mk.finki.ukim.diansproject.model.Exceptions.InvalidArgumentsException;
+import mk.finki.ukim.diansproject.model.Exceptions.InvalidUserCredentialsException;
+import mk.finki.ukim.diansproject.model.User;
+import mk.finki.ukim.diansproject.service.AuthService;
 import mk.finki.ukim.diansproject.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("")
+@RequestMapping()
 public class LoginController {
-    private final UserService userService;
-
-    public LoginController(UserService userService) {
-        this.userService = userService;
+    private final AuthService authService;
+    public LoginController(AuthService authService) {
+        this.authService = authService;
     }
 
-    @GetMapping("")
-    public String getLoginPage(){
+    @GetMapping(value = {"/","/login"})
+    public String getLoginPage() {
         return "login.html";
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam String email,@RequestParam String password){
-        if(userService.isAuthenticated(email,password)){
-            return "redirect:/places";
+    public String login(HttpServletRequest request, Model model) {
+        User user = null;
+
+        try {
+            user = authService.login(request.getParameter("username"), request.getParameter("password"));
+        } catch (InvalidUserCredentialsException | InvalidArgumentsException exception) {
+            model.addAttribute("hasError", true);
+            model.addAttribute("error", exception.getMessage());
+            return "login.html";
         }
-        return "login.html";
-    }
-    @GetMapping("/signin")
-    public String getSigninPage(){
-        return "signin.html";
+
+        request.getSession().setAttribute("user", user);
+        return "redirect:/places";
     }
 
-    @PostMapping("/register")
-    public String register(@RequestParam String name,@RequestParam String email,@RequestParam String password2
-            , @RequestParam String username,@RequestParam String password1){
-        if(!password1.equals(password2)){
-            return "redirect:/signin";
-        }
-        userService.addUser(name,username,email,password1);
-        return "redirect:/";
-    }
+
 }
