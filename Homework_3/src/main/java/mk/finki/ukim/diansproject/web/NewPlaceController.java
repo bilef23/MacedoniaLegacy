3ff09d5f56365ead7fab2661c80f134b21b4a767
risteskamartina <martinaristeska.mr@gmail.com>
2023-12-24 17:32:22 +0100

    Added new place controller

---
+++ Homework_3/src/main/java/mk/finki/ukim/diansproject/web/NewPlaceController.java
@@ -0,0 +1,31 @@
+package mk.finki.ukim.diansproject.web;
+
+import mk.finki.ukim.diansproject.model.CulturalPlace;
+import mk.finki.ukim.diansproject.service.CulturalPlaceService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/new-place")
+public class NewPlaceController {
+    private final CulturalPlaceService culturalPlaceService;
+
+    public NewPlaceController(CulturalPlaceService culturalPlaceService) {
+        this.culturalPlaceService = culturalPlaceService;
+    }
+
+    @GetMapping
+    public String showNewPlaceForm() {
+        return "new_place_form.html";
+    }
+
+    @PostMapping("/save")
+    public String saveNewPlace(@RequestParam String name, @RequestParam String category, @RequestParam String location) {
+        CulturalPlace newPlace = new CulturalPlace(name, category, location);
+        culturalPlaceService.save(newPlace);
+        return "redirect:/places";
+    }
+}
