ef795483cc88423974186eb7bc6725e425121948
bilef23 <92641392+bilef23@users.noreply.github.com>
2023-12-24 20:48:54 +0100

    Repository methods

--- Homework_3/src/main/java/mk/finki/ukim/diansproject/repository/UserRepository.java
+++ Homework_3/src/main/java/mk/finki/ukim/diansproject/repository/UserRepository.java
@@ -8,7 +8,8 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    User searchUserByUsernameAndPassword (String username,String password);
-    User searchUserByEmailAndPassword(String email,String password);
-    Optional< User> findUserByUsername(String username);
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByUsernameAndPassword(String username, String password);
+
 }
