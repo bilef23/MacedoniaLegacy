72e8abb6692037d6ad874b9afe210970f0b207be
bilef23 <92641392+bilef23@users.noreply.github.com>
2023-12-24 22:37:38 +0100

    Removing password encoder

--- Homework_3/src/main/java/mk/finki/ukim/diansproject/DiansProjectApplication.java
+++ Homework_3/src/main/java/mk/finki/ukim/diansproject/DiansProjectApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
 @ServletComponentScan
@@ -15,10 +14,7 @@ public class DiansProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiansProjectApplication.class, args);
     }
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+
 
 
 }
