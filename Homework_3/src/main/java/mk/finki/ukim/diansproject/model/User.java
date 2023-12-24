28dcdcc45e82e85de4742d6d1fdffd78319e654f
bilef23 <92641392+bilef23@users.noreply.github.com>
2023-12-24 20:49:49 +0100

    Changing the @Id annotation on username attribute

--- Homework_3/src/main/java/mk/finki/ukim/diansproject/model/User.java
+++ Homework_3/src/main/java/mk/finki/ukim/diansproject/model/User.java
@@ -2,20 +2,30 @@ package mk.finki.ukim.diansproject.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Data
 @Entity
 @Table(name="users")
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+public class User implements UserDetails {
+
     private String name;
+    @Id
     private String username;
     private String password;
     private String email;
-    @Enumerated
+    @Enumerated(value = EnumType.STRING)
     private Role role;
+    private boolean isAccountNonExpired = true;
+    private boolean isAccountNonLocked = true;
+    private boolean isCredentialsNonExpired =  true;
+    private boolean isEnabled = true;
+
 
     public User( String name, String username, String password, String email, Role role) {
         this.name = name;
@@ -26,5 +36,30 @@ public class User {
     }
 
     public User() {
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(role);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
     }
 }
