1a987bce3f483848b1e6bc891486bebab26a93e4
bilef23 <92641392+bilef23@users.noreply.github.com>
2023-12-24 20:48:17 +0100

    Changing userService methods

--- Homework_3/src/main/java/mk/finki/ukim/diansproject/service/UserService.java
+++ Homework_3/src/main/java/mk/finki/ukim/diansproject/service/UserService.java
@@ -1,11 +1,13 @@
 package mk.finki.ukim.diansproject.service;
 
 import mk.finki.ukim.diansproject.model.CulturalPlace;
+import mk.finki.ukim.diansproject.model.Role;
 import mk.finki.ukim.diansproject.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
-public interface UserService {
-    boolean isAuthenticated(String user,String password);
-    void addUser(String name,String username,String email,String password);
+public interface UserService extends UserDetailsService {
+    User register(String username, String password, String repeatPassword, String name, String email, Role role);
+
 }
