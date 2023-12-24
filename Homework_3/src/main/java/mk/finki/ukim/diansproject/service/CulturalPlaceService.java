83c0056a9f17ff43cc8782274bc194773cd66a19
risteskamartina <martinaristeska.mr@gmail.com>
2023-12-24 18:28:56 +0100

    Added save method

--- Homework_3/src/main/java/mk/finki/ukim/diansproject/service/CulturalPlaceService.java
+++ Homework_3/src/main/java/mk/finki/ukim/diansproject/service/CulturalPlaceService.java
@@ -1,6 +1,7 @@
 package mk.finki.ukim.diansproject.service;
 
 import mk.finki.ukim.diansproject.model.CulturalPlace;
+import mk.finki.ukim.diansproject.model.UserReview;
 
 import java.io.IOException;
 import java.util.List;
@@ -10,4 +11,6 @@ public interface CulturalPlaceService {
     List<CulturalPlace> filter(String name,String category,String location) throws IOException, InterruptedException;
     CulturalPlace findById(Long id);
     CulturalPlace findCulturalPlaceByName(String name);
+
+    void save(CulturalPlace newPlace);
 }
