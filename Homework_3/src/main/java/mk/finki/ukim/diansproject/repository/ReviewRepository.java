31e1ba97a21a68df47dd6dd151603c668ca2acf1
risteskamartina <martinaristeska.mr@gmail.com>
2024-01-27 01:23:53 +0100

    Deleted unused methods

--- Homework_3/src/main/java/mk/finki/ukim/diansproject/repository/ReviewRepository.java
+++ Homework_3/src/main/java/mk/finki/ukim/diansproject/repository/ReviewRepository.java
@@ -9,9 +9,6 @@ import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<UserReview, Long> {
-    UserReview findByCulturalPlace_IdAndUser_Username(Long id, String username);
-
-    List<UserReview> findByUser_username(String username);
 
     List<UserReview> findByCulturalPlace_Id(Long id);
 }
